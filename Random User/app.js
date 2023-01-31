733f072fe82656ca136cd7b1a7f509f89590d8d6
Islom <islomjonalimov83@gmail.com>
2023-01-31 04:25:38 +0000

    displayUser function generating

--- Random User/app.js
+++ Random User/app.js
@@ -2,15 +2,24 @@ import get from "./utils/getElement.js";
 import getUser from "./utils/fetchUser.js";
 
 const img = get(".user-img");
-const title = get(".user-title");
 const value = get(".user-value");
+const title = get(".user-title");
 const btn = get(".btn");
 const btns = [...document.querySelectorAll(".icon")];
-console.log(btns);
 
+const displayUser = (person) => {
+    img.src = person.img;
+    value.textContent = person.name;
+    title.textContent = `My name is`;
+    btns[0].classList.add("active");
+    btns.forEach((btn) => {
+        const label = btn.dataset.label;
+    });
+};
 const showUser = async() => {
     // get user from api
     const person = await getUser();
+    displayUser(person);
     console.log(person);
 };
 
