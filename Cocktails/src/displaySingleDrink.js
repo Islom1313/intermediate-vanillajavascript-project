338d460160536e6aa54600063236d9928f8b2060
Islom <islomjonalimov83@gmail.com>
2023-02-04 23:27:02 +0000

    display single drink

--- Cocktails/src/displaySingleDrink.js
+++ Cocktails/src/displaySingleDrink.js
@@ -1,5 +1,19 @@
-const displaySingleDrink = (drink) => {
-    console.log(drink);
+import { hideLoading } from "./toggleLoading.js";
+import get from "./getElement.js";
+
+const displaySingleDrink = (data) => {
+    hideLoading();
+
+    const drink = data.drinks[0];
+    const { strDrinkThumb: image, strDrink: name, strInstruction: desc } = drink;
+    const list = [
+        drink.strIngredient1,
+        drink.strIngredient2,
+        drink.strIngredient3,
+        drink.strIngredient4,
+        drink.strIngredient5,
+    ];
+    console.log(drink, list);
 };
 
 export default displaySingleDrink;
