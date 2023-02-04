3957689c649de5f8ac8c351bd54f8cb8827ce983
Islom <islomjonalimov83@gmail.com>
2023-02-04 23:47:58 +0000

    Cocktail project complete

--- Cocktails/src/displaySingleDrink.js
+++ Cocktails/src/displaySingleDrink.js
@@ -13,6 +13,22 @@ const displaySingleDrink = (data) => {
         drink.strIngredient4,
         drink.strIngredient5,
     ];
+
+    const img = get(".drink-img");
+    const drinkName = get(".drink-name");
+    const description = get(".drink-desc");
+    const ingredients = get(".drink-ingredients");
+
+    img.src = image;
+    document.title = name;
+    drinkName.textContent = name;
+    description.textContent = desc;
+    ingredients.innerHTML = list
+        .map((item) => {
+            if (!item) return;
+            return `<li> <i class="far fa-check-square"></i> ${item}</li>`;
+        })
+        .join("");
     console.log(drink, list);
 };
 
