ed9c246fff558f12b8a53c8353c606a51c730ebb
Islom <islomjonalimov83@gmail.com>
2023-02-13 00:34:23 +0000

    data js add

---
+++ Stripe-menu/app.js
@@ -0,0 +1,20 @@
+import sublinks from "./data.js";
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const closeBtn = document.querySelector(".close-btn");
+const sideWrapper = document.querySelector(".sidebar-wrapper");
+const sidebar = document.querySelector(".sidebar-links");
+
+const linkBtns = [...document.querySelectorAll(".link-btn")];
+const submenu = document.querySelector(".submenu");
+const hero = document.querySelector(".hero");
+const nav = document.querySelector(".nav");
+
+// console.log(linkBtns);
+// hide/show sidebar
+toggleBtn.addEventListener("click", () => {
+  sideWrapper.classList.add("show");
+});
+closeBtn.addEventListener("click", () => {
+  sideWrapper.classList.remove("show");
+});
