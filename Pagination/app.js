c0b44e3b17bab0fa66024f524986658137e6c06f
Islom <islomjonalimov83@gmail.com>
2023-03-12 19:00:28 +0500

    Math.ceil function use

--- Pagination/app.js
+++ Pagination/app.js
@@ -6,8 +6,9 @@ import displayButtons from "./displayFollowers.js";
 const title = document.querySelector(".section-title h1");
 const init = async () => {
   const followers = await fetchFollowers();
-  displayFollowers(followers);
+  displayFollowers(paginate(followers)[0]);
   title.textContent = "pagination";
+  const pages = paginate(followers);
 };
 
 window.addEventListener("load", init);
--- Pagination/paginate.js
+++ Pagination/paginate.js
@@ -1,3 +1,12 @@
-const paginate = () => {};
+const paginate = (followers) => {
+  //   console.log(followers);
+  const itemsPerpage = 10;
+  const numberOfPages = Math.ceil(followers.length / itemsPerpage);
+  const newFollowers = Array.from({ length: numberOfPages }, (_, index) => {
+    const start = index * itemsPerpage;
+    return followers.slice(start, start + itemsPerpage);
+  });
+  return newFollowers;
+};
 
 export default paginate;
