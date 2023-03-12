64b965f88075494dd43c0cce1e858d2d2db52bf0
Islom <islomjonalimov83@gmail.com>
2023-03-12 17:53:05 +0500

    followers

--- Pagination/app.js
+++ Pagination/app.js
@@ -2,3 +2,10 @@ import fetchFollowers from "./fetchFollowers.js";
 import displayFollowers from "./displayFollowers.js";
 import paginate from "./paginate.js";
 import displayButtons from "./displayFollowers.js";
+
+const init = async () => {
+  const followers = await fetchFollowers();
+  console.log(followers);
+};
+
+window.addEventListener("load", init);
