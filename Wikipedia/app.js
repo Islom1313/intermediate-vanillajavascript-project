96df45be55a7eaceb391f90fac0621db0b088522
Islom <islomjonalimov83@gmail.com>
2023-03-13 16:19:20 +0500

    fetch page complete

--- Wikipedia/app.js
+++ Wikipedia/app.js
@@ -1,24 +1,35 @@
 const url =
   "https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=";
 
-const formDom = document.querySelector(".form");
-const inputDom = document.querySelector(".form-input");
-const resultDom = document.querySelector(".results");
+const formDOM = document.querySelector(".form");
+const inputDOM = document.querySelector(".form-input");
+const resultsDOM = document.querySelector(".results");
 
-formDom.addEventListener("submit", (e) => {
-  e.preventDefault;
-
-  const value = inputDom.value;
-
+formDOM.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const value = inputDOM.value;
   if (!value) {
-    resultDom.innerHTML = '<div class="error">please enter valid search </div>';
+    resultsDOM.innerHTML =
+      '<div class="error"> please enter valid search term</div>';
     return;
   }
   fetchPages(value);
 });
 
 const fetchPages = async (searchValue) => {
-  resultDom.innerHTML = '<div class="loading"></div>';
+  resultsDOM.innerHTML = '<div class="loading"></div>';
   try {
-  } catch (error) {}
+    const response = await fetch(`${url}${searchValue}`);
+    const data = await response.json();
+    const results = data.query.search;
+    if (results.length < 1) {
+      resultsDOM.innerHTML =
+        '<div class="error">No Matches Found. Please Try Again </div>';
+    }
+    renderResults(results);
+  } catch (error) {
+    resultsDOM.innerHTML = '<div class="error">There was an error </div>';
+  }
 };
+
+const renderResults = (list) => {};
